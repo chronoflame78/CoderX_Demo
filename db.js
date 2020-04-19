f1b4cb39932a02da09031eec4dda79b0ae7d7d01
nhatnh <nhatnhse05539@gmail.com>
2020-04-19 20:11:10 +0700

    #csrf defense

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
 var adapter = new FileSync('db.json');
 var db = low(adapter);
-db.defaults({ users: [], sessions: [] })
+db.defaults({ users: [], sessions: [], transfers: [] })
   .write();
 module.exports = db;
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var authRoute = require('./routes/auth.route');
 var cookieParser = require('cookie-parser');
 var productRoute = require('./routes/product.route');
 var cartRoute = require('./routes/cart.route');
+var transferRoute = require('./routes/transfer.route');
+var csurf = require('csurf');
 
 var authMiddleware = require('./middlewares/auth.middleware');
 var sessionMiddleware = require('./middlewares/session.middleware');
@@ -22,6 +24,8 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(express.static('public'));
 app.use(sessionMiddleware);
+app.use(csurf({cookie: true}));
+
 
 app.get('/', function (req, res){
 	res.render('index',{
@@ -33,6 +37,8 @@ app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
 app.use('/products', productRoute);
 app.use('/cart', cartRoute);
+app.use('/transfer', authMiddleware.requireAuth, transferRoute);
+
 
 
 app.listen(port, function(){
